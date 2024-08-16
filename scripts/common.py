d12e8a5313cc3c4cc1564600a86cb3bcf55def88
Qijia Liu <liumeo@pku.edu.cn>
2024-08-16 12:20:14 -0400

    libexpat

--- scripts/common.py
+++ scripts/common.py
@@ -26,16 +26,18 @@ def patch(project: str, src: str, dst: str | None = None):
 
 
 class Builder:
-    def __init__(self, name: str, options: list[str] | None=None):
+    def __init__(self, name: str, options: list[str] | None=None, src='.'):
         self.name = name
         self.root = os.getcwd()
         self.destdir = f'{self.root}/build/{self.name}'
         self.options = options or []
+        self.src = src
 
     def configure(self):
         os.chdir(f'{self.root}/{self.name}')
         ensure('emcmake', ['cmake',
             '-B', 'build', '-G', 'Ninja',
+            '-S', self.src,
             '-DBUILD_SHARED_LIBS=OFF',
             f'-DCMAKE_INSTALL_PREFIX={INSTALL_PREFIX}',
             '-DCMAKE_BUILD_TYPE=Release',
